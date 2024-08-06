f8ba9a2c8cecd417fcf73c2864a009049a41ce4a
1bdou <149634399+1bdou@users.noreply.github.com>
2024-08-06 09:56:05 +0100

    added the delimiters parameter

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: acharik <acharik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 06:49:49 by acharik           #+#    #+#             */
-/*   Updated: 2024/08/06 06:50:48 by acharik          ###   ########.fr       */
+/*   Updated: 2024/08/06 09:54:19 by acharik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,34 @@ static char	is_delimiter(char c, char *delimiters)
 	return (0);
 }
 
-static int	words_count(char *str, char *delimiters)
+int	skip_delimiters(const char *str, int *i, char *delimiters, int flag)
+{
+	if (flag)
+	{
+		while (str[*i + 1] && is_delimiter(str[*i], delimiters))
+			(*i)++;
+	}
+	else
+	{
+		while (str[*i + 1] && !is_delimiter(str[*i], delimiters))
+			// nor delimters
+			(*i)++;
+	}
+	return (*i);
+}
+
+static int	words_count(const char *str, char *delimiters)
 {
 	int	i;
 	int	words;
 
 	words = 0;
 	i = 0;
-	while (is_delimiter(str[i], delimiters))
-		i++;
 	while (str[i])
 	{
-		if (is_delimiter(str[i], delimiters) || !str[i + 1])
+		if (!is_delimiter(str[i], delimiters))
 		{
-			while (is_delimiter(str[i], delimiters))
-				i++;
+			skip_delimiters(str, &i, delimiters, 0);
 			words++;
 		}
 		i++;
@@ -48,14 +61,16 @@ static int	words_count(char *str, char *delimiters)
 	return (words);
 }
 
-static void	store_data(char **dest, char *src, int str_size, char *delimiters)
+static void	store_data(char **dest, const char *src, int str_size, char is_end)
 {
+	if (!is_end)
+		str_size++;
 	*dest = malloc(sizeof(char) * (str_size + 1));
 	ft_memmove(*dest, src, str_size);
 	(*dest)[str_size] = '\0';
 }
 
-char	**ft_split(char *str, char *delimiters)
+char	**ft_split(const char *str, char *delimiters)
 {
 	int		i;
 	int		j;
@@ -66,20 +81,18 @@ char	**ft_split(char *str, char *delimiters)
 	i = 0;
 	j = 0;
 	split = malloc(sizeof(char *) * (words_count(str, delimiters) + 1));
-	start = skip_delimiters(str, i, delimiters);
+	start = skip_delimiters(str, &i, delimiters, 1);
 	while (str[i])
 	{
-		if (is_delimiter(str[i], delimiters))
+		if (is_delimiter(str[i], delimiters) || !str[i + 1])
 		{
 			str_size = i - start;
-			store_data(&(split[j]), &(str[start]), str_size, delimiters);
+			store_data(&(split[j]), &(str[start]), str_size, str[i + 1]);
 			j++;
-			start = skip_delimiters(str, i, delimiters);
+			start = skip_delimiters(str, &i, delimiters, 1);
 		}
 		i++;
 	}
-	str_size = i - start;
-	store_data(&(split[j++]), &(str[start]), str_size, delimiters);
 	split[j] = NULL;
 	return (split);
 }
