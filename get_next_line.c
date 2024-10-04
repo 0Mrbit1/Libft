639dac5d0784edc1fdffb64641c4452e1954e2e3
Mr.Bit <149634399+0Mrbit1@users.noreply.github.com>
2024-10-04 14:58:14 +0100

    improved  gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,111 +1,170 @@
-#include "../../include/minishell.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-static char *with_line (char *last_line  , char *line  , short *line_found )
+//#define BUFFER_SIZE 42
+
+int ft_strlen(char *str)
 {
-    char *final_line ; 
-    
-    if (!last_line)
-        final_line = line  ; 
-    else
-    {
-        final_line = ft_strjoin(last_line , line );
-        free(line);
-        free(last_line);
-    }
-    *line_found = 1 ; 
-    return final_line; 
+    int i ;
+
+    i = 0 ;
+    while(str[i])
+        i++;
+    return i;
 }
 
-static char *without_line (char **buffer , char *last_line   , char *line)
+char *ft_strjoin(char *chunk1 , char *chunk2)
 {
-    char *final_line ; 
-
-    free(*buffer);
-    *buffer = NULL ;
-    if (!last_line)
-        final_line = line ;
-    else
-    {
-        final_line = ft_strjoin(last_line , line );
-        free(line);
-        free(last_line);
-    }
-    return final_line;
-}
-
-static char *build_line(char **buffer , int bytes_read , char *last_line , short *line_found )
-{
-    int i;
-    char *line;
+    int i ; 
+    int j;
+    char *new_str; 
 
     i = 0;
-    line = (char*)malloc((bytes_read + 1 )* sizeof(char) );
-    if (!line)
-        return NULL;
-    while((*buffer)[i])
+    j = 0;
+    new_str = (char*)malloc( sizeof(char)  * ( ft_strlen(chunk1) + ft_strlen(chunk2)  +1 )    ) ;
+    if(!new_str)
+        exit(1); 
+    if (!new_str)
+        exit(1);
+    if (!new_str)
+        return NULL; 
+    while(chunk1[i])
     {
-        line[i] = (*buffer)[i] ;
-        if ((*buffer)[i] == '\n')
+        new_str[j] = chunk1[i] ; 
+        j++;
+        i++;
+    }
+    i = 0 ;
+    while(chunk2[i])
+    {
+        new_str[j] = chunk2[i] ; 
+        i++;
+        j++;
+    }
+
+    new_str[j] = '\0' ;
+    free(chunk1) ;
+    free(chunk2) ;
+    return new_str ;
+}
+
+char *ft_strdup(char *str)
+{
+    int i;
+    char *new_str;
+
+    i = 0;
+    new_str = malloc(ft_strlen(str) +  1 );
+    if(!new_str)
+        exit(1);
+    if(!new_str)
+        exit(1);
+    while(str[i])
+    {
+        new_str[i] = str[i] ; 
+        i++;
+    }
+    new_str[i] = '\0' ; 
+    return new_str; 
+}
+
+char*  build_line(char *last_line , char *buffer , short *line_found)
+{
+    int  i;
+    char *line;
+    char *new_line; 
+
+    i = 0;
+    line = (char*)malloc(sizeof(char)*(ft_strlen(buffer) + 1) ); 
+    if (!line)
+        exit(1);
+    if(!line)
+        exit(1);
+    while(buffer[i])
+    {
+        line[i] = buffer[i]; 
+        if(line[i] == '\n')
         {
-            ft_memmove(*buffer ,  &((*buffer)[i+1])    , bytes_read - i) ; 
-            line[++i] = '\0';
-            if ((*buffer)[i] == '\0')
-            {
-                free(*buffer);
-                *buffer = NULL ;
-            }
-            return with_line (last_line  , line  , line_found ); 
+            *line_found = i + 1; 
+            line[++i] = '\0'; 
+             if (!last_line) 
+                    return line;
+            new_line = ft_strjoin(last_line , line); 
+            return new_line; 
         }
         i++;
     }
-    line[i] = '\0';
-    return without_line (buffer , last_line , line);
+    line[i] = '\0'; 
+    if (!last_line) 
+        return line; 
+   
+    new_line = ft_strjoin(last_line , line); 
+    return new_line; 
 }
 
-char *read_file(int fd , char *buffer  , ssize_t *bytes_read)
+char *get_next_line(int fd )
 {
-    char *clean;
+    if (fd < 0)
+        return NULL ; 
+    int bytes_read ;
+    char *line ; 
+    short line_found; 
+    static char  *buffer;
+    char *new_buffer; 
 
+    line_found = 0;
+    line = NULL;
     if (buffer)
     {
-        *bytes_read = ft_strlen(buffer);
-        return buffer;
-    }
-    buffer = (char*)malloc((BUFFER_SIZE +1) * sizeof(char)  );
-    if (!buffer)
-        return  NULL;
-    *bytes_read = read( fd , buffer , BUFFER_SIZE ) ;
-    buffer[*bytes_read] = '\0';
-    clean = buffer ;
-    buffer = ft_strdup(buffer) ;
-    free(clean); 
-    if (!*bytes_read)
-    {
-        free(buffer);
-        buffer = NULL;
-    }
-    return buffer;
-}
-
-char *get_next_line(int fd)
-{
-   ssize_t bytes_read;
-   static char *buffer;
-   char *line;
-   short line_found ; 
-  
-   line_found = 0;
-   line = NULL;
-   while (1)
-   {
-        buffer = read_file(fd , buffer  , &bytes_read); 
-        if (buffer && !line_found)
+        line = build_line(line , buffer , &line_found );    
+        if(line_found)
         {
-            line = build_line(&buffer , bytes_read , line , &line_found );
-            if (line_found)
-                return line ;
-        } 
-        else 
-            return line ;
-   }
+            new_buffer = ft_strdup (     &(buffer[line_found])   ) ;
+            free(buffer);
+            buffer = new_buffer;
+            if (!(*buffer))
+            {
+                free(buffer);
+                buffer = NULL ; 
+            } 
+            return line; 
+        }
+    }
+    free(buffer);
+    buffer = malloc(sizeof(char)*(BUFFER_SIZE + 1 ) ) ; 
+
+    while (1)
+    {
+        bytes_read = read(fd , buffer , BUFFER_SIZE );
+        if (bytes_read < 0)
+        {
+            free(buffer);
+            return NULL ; 
+        }
+        buffer[bytes_read] = '\0'; 
+        if (!bytes_read)
+        {
+            free(buffer) ;
+            buffer = NULL ; 
+            return line ; 
+        }
+        line = build_line(line , buffer , &line_found);
+        if(line_found)
+        {
+            new_buffer = ft_strdup(     &(buffer[line_found])   ) ;  
+            free(buffer);
+            buffer = new_buffer;
+            if (!*buffer)
+            {
+                free(buffer);
+                buffer = NULL ; 
+            }
+            return line ; 
+        }
+    }
+    free(buffer);
+    return line ; 
+
 }
